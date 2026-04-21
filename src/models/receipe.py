e2b7fad6c283e05acca934c16b94b45ef1ac5008
amrutakhot16 <66746399+amrutakhot16@users.noreply.github.com>
2026-04-21 01:15:15 -0400

    Update receipe.py

--- src/models/receipe.py
+++ src/models/receipe.py
@@ -1,19 +1,8 @@
 class Recipe:
-    def __init__(self, name, calories, protein, ingredients, diet, meal_type):
+    def __init__(self, name, calories, protein, ingredients, diet):
         self.name = name
         self.calories = calories
         self.protein = protein
         self.ingredients = ingredients
         self.diet = diet
-        self.meal_type = meal_type
 
-    @staticmethod
-    def from_dict(data):
-        return Recipe(
-            name=data.get("name", ""),
-            calories=data.get("calories", 0),
-            protein=data.get("protein", 0),
-            ingredients=data.get("ingredients", []),
-            diet=data.get("diet", "any"),
-            meal_type=data.get("meal_type", "dinner")
-        )
