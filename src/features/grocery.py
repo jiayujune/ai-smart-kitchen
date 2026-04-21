1d92d52b2eba7ca8490a70f59c8b42a05836f78b
amrutakhot16 <66746399+amrutakhot16@users.noreply.github.com>
2026-04-21 01:13:25 -0400

    Update grocery.py

--- src/features/grocery.py
+++ src/features/grocery.py
@@ -2,7 +2,9 @@ def generate(meal_plan):
     grocery = {}
 
     for recipe in meal_plan.values():
-        for item in recipe.ingredients:
+        ingredients = recipe["ingredients"]
+
+        for item in ingredients:
             grocery[item] = grocery.get(item, 0) + 1
 
     return grocery
