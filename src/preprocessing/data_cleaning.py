c53b415cef151bb5add131bbf110b22d46e8aeeb
amrutakhot16 <66746399+amrutakhot16@users.noreply.github.com>
2026-04-21 01:15:40 -0400

    Update data_cleaning.py

--- src/preprocessing/data_cleaning.py
+++ src/preprocessing/data_cleaning.py
@@ -1,32 +1,50 @@
 import pandas as pd
 import ast
-from models.receipe import Recipe
+
 
 def safe_eval(x):
     try:
         return ast.literal_eval(x)
     except:
-        return []  # If parsing fails, return an empty list
+        return []
 
-def load_data(input_path):
+
+def clean_data(input_path, output_path):
     df = pd.read_csv(input_path)
 
+    print("Original columns:", df.columns)
+
+    df = df[["name", "ingredients", "nutrition"]]
+    df = df.dropna()
+
+    df["ingredients"] = df["ingredients"].apply(safe_eval)
+    df["nutrition"] = df["nutrition"].apply(safe_eval)
+
+    df["calories"] = df["nutrition"].apply(lambda x: x[0] if len(x) > 0 else 0)
+    df["protein"] = df["nutrition"].apply(lambda x: x[4] if len(x) > 4 else 0)
+
+    #Save cleaned data
+    df.to_csv(output_path, index=False)
+
+    print(f" Cleaned data saved to {output_path}")
+
+def load_data(path):
+    df = pd.read_csv(path)
+
     df["ingredients"] = df["ingredients"].apply(
-        lambda x: safe_eval(x) if isinstance(x, str) else x
+        lambda x: ast.literal_eval(x) if isinstance(x, str) else x
     )
 
-    recipes = []
+    return df.to_dict("records")
 
-    for row in df.to_dict("records"):
-        recipes.append(
-            Recipe(
-                name=row["name"],
-                calories=row["calories"],
-                protein=row.get("protein", 0),
-                ingredients=row["ingredients"],
-                diet=row.get("diet", "any"),
-                meal_type=row.get("meal_type", "dinner")
-            )
-        )
+def detect_diet(ingredients):
+    non_veg_keywords = [
+        "chicken", "beef", "pork", "fish",
+        "meat", "crab", "turkey", "lamb"
+    ]
 
-    return recipes
+    for ingredient in ingredients:
+        if any(word in ingredient.lower() for word in non_veg_keywords):
+            return "non-veg"
+
+    return "vegetarian"
