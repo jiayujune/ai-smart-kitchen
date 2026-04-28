43fa77157a17b4b3809d282f07e838f2ccaa99e4
amruta <amrutakhot1606@gmail.com>
2026-04-28 01:38:15 -0400

    Move csp logic into code/csp module

--- src/models/receipe.py
+++
@@ -1,7 +0,0 @@
-class Recipe:
-    def __init__(self, name, calories, protein, ingredients, diet):
-        self.name = name
-        self.calories = calories
-        self.protein = protein
-        self.ingredients = ingredients
-        self.diet = diet
